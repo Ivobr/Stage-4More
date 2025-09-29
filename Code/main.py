de06ab1d185f4ee1ef77dd1bbad9b1444bb4684e
ivobr <ivobruinsma23@gmail.com>
2025-09-29 13:59:17 +0200

    Code layout

--- Code/main.py
+++ Code/main.py
@@ -1,33 +1,62 @@
 from fairino import Robot
 import time
-#        x   y   z   rx   ry  rz
-joint_point = [15, 20, 25, 145, 23, 25]
-type = "obj"
+import random
 
-tool = user = 0
-vel = 50 # % of max-speed
-
-# robot = Robot.RPC('192.168.58.2')
-
-# robot.SetSpeed(20)
-
-def input():
-    print("Sensor leest hoog")
+sensor = False
+band = False
+joint_pos = [-11.904, -99.669, 117.473, -108.616, -91.726, 74.256]  # random start position
+rest_pos = [10,10,10,10,10,10]
 
 
-input()
+# robot = Robot.RPC('192.168.52.2')
 
-def MoveTo(type, cords):
-    match type:
-        case "band":
-            print("moving above obj")
-            #robot.MoveJ(joint_pos=joint_point, tool=tool, user=user, vel=vel)
+def rest():
+    if joint_pos == rest_pos:
+        time.sleep(1)
+    else:
+        move(rest_pos)
 
-            type = "obj"
-        case "obj":
-            print("localize flower")
-            time.sleep(1)
-            print("going down to pick flower")
-    print(cords)
 
-MoveTo(type=type, cords=joint_point)
+def inputsensor():
+    global sensor
+    global joint_pos
+    print("Reading sensor")
+    # sensor.read(type shit)
+    num = random.randint(1, 10)
+    print(num)
+    if num == 4:
+        sensor = True
+        joint_pos = [0, 0, 0, 0, 0, 0]  # band pos
+
+
+def move(moveTo):
+    global joint_pos
+    global band
+    print("Moving to")
+    print(moveTo)
+    if not band:
+        print("Move to band")
+        # rtn = robot.MoveJ(joint_pos=moveTo, tool=tool, user=user, exaxis_pos=epos, blendT=blendT, offset_flag=flag, offset_pos=offset_pos1)
+        band = True
+    else:
+        print("Move to flower")
+        # err, pose = robot.GetActualTCPPose(0) # pos = [x,y,z,rx,ry,rz]
+        # pose[2] -= 20.0 # move down 20 mm
+        # rtn = robot.MoveL(cart_pos=pose, tool=0, user=0, vel=50, acc=50)
+
+
+    print("moved")
+    joint_pos = moveTo
+
+def main():
+    print(joint_pos)
+    while not sensor:
+        rest()
+        inputsensor()
+    print("Done")
+    move(joint_pos)
+
+
+if __name__ == '__main__':
+    main()
+    # robot.CloseRPC()
