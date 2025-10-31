cc6e287be3911d8963437572cff454cc86ec3dc2
ivobr <ivobruinsma23@gmail.com>
2025-10-31 11:49:49 +0100

    Damn shawty thi s shi works
    Er wordt een lijst gemaakt met alle assen waardes en de hoogste wordt gepakt en door de switch gegooid. Hierdoor wordt altijd de grootste lezing gepakt en niet de eerste welke de for loop tegen komt

--- ControllerCode/inputTest.py
+++ ControllerCode/inputTest.py
@@ -1,14 +1,16 @@
 import pygame
+import time
 
 pygame.init()
 pygame.joystick.init()
 joystick = pygame.joystick.Joystick(0)
 joystick.init()
 
-Xas = Yas = Zas = S5 = False
-lastValY = lastValZ = lastValX = lastValS5 = 0
+Xas = Yas = ZasUp = ZasDown = S5 = S4 = False
+lastValX = lastValY = lastValZup = lastValZdown = lastValS5 = lastValS4 = 0
 
 values = [0,0,0,0,0,0]
+TrueValues = [0,0,0,0,0,0]
 
 
 def calc(axis_val):
@@ -20,77 +22,109 @@ while True:
             running = False
 
     axes = joystick.get_numaxes()
+    # time.sleep(1)
     for i in range(axes):
         axis_val = joystick.get_axis(i)
-        match i:
-            case 0:
-                if axis_val > 0.1:
-                    if axis_val - lastValX > 0.1 or axis_val - lastValX < -0.1:
-                        # JOG(1, 1, axis_val, 2)
-                        print("Update")
-                        lastValX = axis_val
-                    Xas = True
-                    print(axis_val)
-                elif axis_val < -0.1:
-                    axis_val = abs(axis_val)
-                    if axis_val - lastValX > 0.1 or axis_val - lastValX < -0.1:
-                        # JOG(1,0,axis_val, 2)
-                        lastValX = axis_val
-                    Xas = True
-                    print(axis_val)
-                else:
-                    Xas = False
-            case 1:
-                if axis_val > 0.1:
-                    if axis_val - lastValY > 0.1 or axis_val - lastValY < -0.1:
-                        # JOG(2, 1, axis_val, 2)
-                        print("Update")
-                        lastValY = axis_val
-                    Yas = True
-                    print(axis_val)
-                elif axis_val < -0.1:
-                    axis_val = abs(axis_val)
-                    if axis_val - lastValY > 0.1 or axis_val - lastValY < -0.1:
-                        # JOG(2, 0, axis_val, 2)
-                        lastValY = axis_val
-                    Yas = True
-                    print(axis_val)
-                else:
-                    Yas = False
-            case 2:
+        if axis_val < 0:
+            TrueValues[i] = axis_val
+            axis_val = abs(axis_val)
+        else:
+            TrueValues[i] = axis_val
 
-                if axis_val > 0.1:
-                    if axis_val - lastValS5 > 0.1 or axis_val - lastValS5 < -0.1:
+        if i < 4:
+            values[i] = axis_val
 
-                        print(axis_val)
-                        lastValS5 = axis_val
-                    S5 = True
-                elif axis_val < -0.1:
-                    axis_val = abs(axis_val)
-                    if axis_val - lastValS5 > 0.1 or axis_val - lastValS5 < -0.1:
 
-                        print(axis_val)
-                        lastValS5 = axis_val
-                    S5 = True
-                else:
-                    S5 = False
-            case 4:
-                if axis_val > -0.9:
-                    axis_val = axis_val + 1
-                    # rtn = robot.StartJOG(2,3,0, 10000)
+
+    element = max(values)
+    i = values.index(element)
+    axis_val = TrueValues[i]
+    match i:
+        case 0:
+            if axis_val > 0.1:
+                if axis_val - lastValX > 0.1 or axis_val - lastValX < -0.1:
+                    print("Linker=,", axis_val)
+                    lastValX = axis_val
+                Xas = True
+            elif axis_val < -0.1:
+                axis_val = abs(axis_val)
+                if axis_val - lastValX > 0.1 or axis_val - lastValX < -0.1:
+                    print("Linker links=,", axis_val)
+                    lastValX = axis_val
+                Xas = True
+            else:
+                Xas = False
+        case 1:
+            if axis_val > 0.1:
+                if axis_val - lastValY > 0.1 or axis_val - lastValY < -0.1:
+                    print("Linker omlaag=,", axis_val)
+                    lastValY = axis_val
+                Yas = True
+            elif axis_val < -0.1:
+                axis_val = abs(axis_val)
+                if axis_val - lastValY > 0.1 or axis_val - lastValY < -0.1:
+                    lastValY = axis_val
+                    print("Linker omhoog =,", axis_val)
+                Yas = True
+            else:
+                Yas = False
+        # rechter joystick
+        case 2:
+            if axis_val > 0.1:
+                if axis_val - lastValS5 > 0.1 or axis_val - lastValS5 < -0.1:
+                    print("Rechter hori=,", axis_val)
+                    lastValS5 = axis_val
+                S5 = True
+            elif axis_val < -0.1:
+                axis_val = abs(axis_val)
+                if axis_val - lastValS5 > 0.1 or axis_val - lastValS5 < -0.1:
+                    print("Rechter hori=,", axis_val)
+                    lastValS5 = axis_val
+                S5 = True
+            else:
+                S5 = False
+
+        case 3:
+            if axis_val > 0.1:
+                if axis_val - lastValS4 > 0.1 or axis_val - lastValS4 < -0.1:
+                    print("Rechter hoog=,", axis_val)
+                    lastValS4 = axis_val
+                S4 = True
+            elif axis_val < -0.1:
+                print("neg")
+                axis_val = abs(axis_val)
+                if axis_val - lastValS4 > 0.1 or axis_val - lastValS4 < -0.1:
                     print(axis_val)
-                    calc(axis_val)
-                    Zas = True
-                else:
-                    Zas = False
-            case 5:
-                if axis_val > -0.9:
-                    # rtn = robot.StartJOG(2,3,1,10000)
-                    Zas = True
-                    print(axis_val)
-                else:
-                    Zas = False
-        if Xas == False and Yas == False and Zas == False and S5 == False:
-            # robot.StopJOG(3)
-            lastValZ = lastValY = lastValX = lastValS5 = 0
-            print("little big ")
+                    lastValS4 = axis_val
+                S4 = True
+            else:
+                S4 = False
+        # L2
+        case 4:
+            if axis_val > -0.9:
+                print("axis before +1 = ", axis_val)
+                axis_val += 1
+                print("Axis after +1 = ", axis_val)
+                print("Last before som = ", lastValZdown)
+                print("The if statement= ", axis_val - lastValZdown)
+                if axis_val - lastValZdown > 0.2 or axis_val - lastValZdown < -0.2:
+                    lastValZdown = axis_val
+                    print("LAst= ", lastValZdown)
+                    print("Axis= ", axis_val)
+                ZasDown = True
+            else:
+                ZasDown = False
+        # R2
+        case 5:
+            if axis_val > -0.9:
+                axis_val += 1
+                if axis_val - lastValZup > 0.2 or axis_val - lastValZup < -0.2:
+                    lastValZup = axis_val
+                ZasUp = True
+
+            else:
+                ZasUp = False
+    if  Xas == False and Yas == False and ZasUp == False and ZasDown == False and S5 == False and S4 == False:
+        # robot.StopJOG(3)
+        lastValZ = lastValY = lastValX = lastValS5 = 0
+        print("little big ")
