36a354b21a3af087ae289e85065d7c79c121f018
ivobr <ivobruinsma23@gmail.com>
2025-11-04 10:38:55 +0100

    Soepel met calc

--- TestCodes/ControllerMove.py
+++ TestCodes/ControllerMove.py
@@ -4,33 +4,34 @@
 
 from fairino import Robot
 import time
+import calc
+x = 0
+y = 0
+z = 0
+r = 0
+a = 0
 
 robot = Robot.RPC('192.168.178.23')
 blendT=500.0
 sleep = 0.0
 
-start_point = [[625, -600.945, -0.246, -179, -0.404, 41.868],
+start_point = [[-625, 600.945, -0.246, -179, -0.404, 41.868],
                [625, 334.438, -0.246, -179, -0.404, 41.868],
                [225, 534.438, -0.246, -179, -0.404, 41.868]]
 
 rtn = robot.MoveCart(desc_pos=start_point[0], tool=0, user=0, vel=50, blendT=blendT)
 print(rtn)
-# time.sleep(sleep)
-# rtn = robot.MoveCart(desc_pos=start_point[1], tool=0, user=0, vel=75, blendT=blendT)
-# print(rtn)
-# time.sleep(sleep)
-# rtn = robot.MoveCart(desc_pos=start_point[2], tool=0, user=0, vel=100, blendT=blendT)
-# print(rtn)
-#
-#
-# rtn = robot.MoveCart(desc_pos=start_point[2], tool=0, user=0, vel=50, blendT=blendT)
-# print(rtn)
-# time.sleep(sleep)
-# rtn = robot.MoveCart(desc_pos=start_point[1], tool=0, user=0, vel=75, blendT=blendT)
-# print(rtn)
-# time.sleep(sleep)
-# rtn = robot.MoveCart(desc_pos=start_point[0], tool=0, user=0, vel=100, blendT=blendT)
-# print(rtn)
+
+rtn, pos = robot.GetActualTCPPose()
+print(pos)
+a = calc.getA(pos[0], pos[1])
+r = calc.getR(pos[0], pos[1])
+x = pos[0]
+y = pos[1]
+z = pos[2]
+print(a, r)
+print(calc.rIncrement(r,a))
+
 i = 0
 time.sleep(5)
 
@@ -39,12 +40,13 @@ rtn, pos = robot.GetActualTCPPose()
 vel = 50
 
 #simuleert controller joystick input
-while i < 10:
+while i < 100:
     print("Nu start")
 
 
     #als vel berekent wordt kan deze elke keer meegegeven worden maybe
-    pos[1] += 100
+    pos[0] += 10
+    pos[1] = calc.calcPoint(pos[0], r, 0)
     rtn = robot.MoveCart(desc_pos=pos, tool=0, user=0, vel=vel, blendT=blendT)
     print(rtn)
     time.sleep(sleep)
